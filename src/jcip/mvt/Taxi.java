51bd173c4cd5664b3b7b5fee9625271bad3468b8
Thanh <thanhvv@gmail.com>
2017-05-19 13:18:15 -0700

    Synchronize the operations on data of taxi. Thus, make the class thread safe.

--- src/jcip/mvt/Taxi.java
+++ src/jcip/mvt/Taxi.java
@@ -15,25 +15,36 @@ public class Taxi extends Thread{
 	}
 	
 	public Point getLocation(){
-		return location;
+		synchronized (location) {
+			return location;
+		}
 	}
 	
 	public void setLocation(Point location) {
-		this.location = location;		
+		synchronized (this.location) {
+			this.location = location;
+		}
 	}
 	
 	public void setDestination(Point destination) {
-		this.destination = destination;
+		synchronized (this.destination) {
+			this.destination = destination;
+		}
 	}
 	
 	public void run() {
 		while (!location.equals(destination)) {
 			try {
 				sleep(1);
-				int x = location.getX();
-				int y = location.getY();
-				int x1 = destination.getX();
-				int y1 = destination.getY();
+				int x, y, x1, y1;
+				synchronized (location) {
+					x = location.getX();
+					y = location.getY();
+				}
+				synchronized (destination) {
+					x1 = destination.getX();
+					y1 = destination.getY();
+				}
 				if (x1 > x) x++;
 				else if (x1 < x) x--;
 				if (y1 > y) y++;
