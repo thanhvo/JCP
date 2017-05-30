389cd0cca461b2d2f1c19e0a19c1f1cb513b3a05
Thanh <thanhvv@gmail.com>
2017-05-30 13:19:26 -0700

    Bounded buffer using explicit condition variables.

---
+++ src/jcip/ConditionBoundedBuffer.java
@@ -0,0 +1,51 @@
+package jcip;
+
+import jcip.annotations.*;
+import java.util.concurrent.locks.*;
+
+@ThreadSafe
+public class ConditionBoundedBuffer<T> {
+	private final int BUFFER_SIZE = 10;
+	protected final Lock lock = new ReentrantLock();
+	// CONDITION PREDICATE: notFull (count < items.length)
+	private final Condition notFull = lock.newCondition();
+	// CONDITION PREDICATE: notEmpty (count > 0)
+	private final Condition notEmpty = lock.newCondition();
+	@GuardedBy("lock")
+	private final T[] items = (T[]) new Object[BUFFER_SIZE];
+	@GuardedBy("Lock") private int tail, head, count;
+	
+	// BLOCKS-UNTIL: notFull
+	public void put(T x) throws InterruptedException {
+		lock.lock();
+		try {
+			while(count == items.length) 
+				notFull.await();
+			items[tail] = x;
+			if (++tail == items.length)
+				tail = 0;
+			++count;
+			notEmpty.signalAll();
+		} finally {
+			lock.unlock();
+		}
+	}
+	
+	// BLOCKS-UNTIL: notEmpty
+	public T take() throws InterruptedException {
+		lock.lock();
+		try {
+			while (count == 0)
+				notEmpty.await();
+			T x = items[head];
+			items[head] = null;
+			if (++head == items.length)
+				head = 0;
+			--count;
+			notFull.signal();
+			return x;
+		} finally {
+			lock.unlock();
+		}
+	}
+}
